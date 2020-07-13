faa82ef2033a693d68654a56b98d64166de2d7eb
Ethan Gough <dante1498@gmail.com>
2020-07-13 11:27:40 +0100

    Push

--- scripts/scaper.py
+++ scripts/scaper.py
@@ -4,5 +4,8 @@ import requests
 url = 'https://store.playstation.com/en-gb/home/games'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='ember1151')
 
 print (content)
