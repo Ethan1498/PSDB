9147937d6d4c8461d8e292dfd81dee59e5dcb4aa
Ethan Gough <dante1498@gmail.com>
2020-07-13 11:28:41 +0100

    Push update to web scraper

--- scripts/scaper.py
+++ scripts/scaper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://store.playstation.com/en-gb/home/games'
+page = requests.get(url)
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 soup = BeautifulSoup(page.content, 'html.parser')
