aede997f1853dfe492c6a18e90b7c0e8fb443026
Chris <chrhac13@gmail.com>
2020-07-22 12:12:54 +0100

    Total pages != 0

--- scripts/pagination.php
+++ scripts/pagination.php
@@ -24,6 +24,10 @@
     $result = mysqli_query($conn,$total_pages_sql);
     $total_rows = mysqli_fetch_array($result)[0];
     $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+    if ($total_pages < 1) {
+        $total_pages = 5;
+    }
         
     $sql = "SELECT * FROM table LIMIT $offset, $no_of_records_per_page";
     $res_data = mysqli_query($conn,$sql);
