9a3f440d92123a2e91568e216aa4ec913012ade8
Chris <chrhac13@gmail.com>
2020-07-24 17:26:13 +0100

    pagination styling

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <div class="col-12 text-center">
         <ul class="pagination">
             <!-- hidden class to be updated once a db is created -->
-            <li class="<?php if($pageno <= 1 ){ echo 'hidden'; }?>"><a href="?pageno=1">First</a></li>
-            <li class="<?php if($pageno <= 1 ){ echo "hidden"; }?>"><a href="<?php  echo "?pageno=".($pageno - 1); ?>"><?php echo $pageno-1?></a></li>
-            <li class="disabled"><a href="#"><?php echo $pageno ?></a></li>
-            <li class="<?php if($pageno >= $total_pages ){ echo 'hidden'; }?>"><a href="<?php echo "?pageno=".($pageno + 1); ?>"><?php echo $pageno+1 ?></a></li>
-            <li class="<?php if($pageno >= $total_pages ){ echo 'hidden'; }?>"><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+            <li class="pagenum <?php if($pageno <= 1 ){ echo 'hidden'; }?>"><a class="page" href="?pageno=1">First</a></li>
+            <li class="pagenum <?php if($pageno <= 1 ){ echo "hidden"; }?>"><a class="page" href="<?php  echo "?pageno=".($pageno - 1); ?>"><?php echo $pageno-1?></a></li>
+            <li class="pagenum disabled"><a class="page" href="#"><?php echo $pageno ?></a></li>
+            <li class="pagenum <?php if($pageno >= $total_pages ){ echo 'hidden'; }?>"><a class="page" href="<?php echo "?pageno=".($pageno + 1); ?>"><?php echo $pageno+1 ?></a></li>
+            <li class="pagenum <?php if($pageno >= $total_pages ){ echo 'hidden'; }?>"><a class="page" href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
         </ul>
     </div>
 </div>
