4c7996811cd170bc6c33d54500cba0cda67daabf
Chris <chrhac13@gmail.com>
2020-07-27 12:07:45 +0100

    game-view prints game price and title

---
+++ game-view.php
@@ -0,0 +1,21 @@
+<?php include "scripts/pagination.php";?>
+<?php include "sources/head.php"; ?>
+<?php include "sources/nav.php";?>
+
+<?php 
+    $id = $_GET["ID"];
+    $conn = mysqli_connect($host,$user,$password,$database);
+    $sql = "SELECT * FROM games WHERE id = $id";
+    $result = mysqli_query($conn,$sql);
+    while($row = mysqli_fetch_assoc($result)){     
+        echo $row["id"]; 
+        echo "<br>";
+        echo $row["title"]; 
+        echo "<br>";
+        echo "£".$row["price"]." ";   
+    }
+?>
+
+
+
+<?php include "sources/footer.php";?>
