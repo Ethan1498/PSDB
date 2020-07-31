6828d766e397ac4bffd3500eca6a7f0881aea3c0
Ethan Gough <dante1498@gmail.com>
2020-07-31 15:24:50 +0100

    Basic footer no styling

--- sources/footer.php
+++ sources/footer.php
@@ -1,8 +1,30 @@
 <footer class="fixed-bottom text-center black-bg">
-        <div class="row">
-            <div class="col-12">
-            </div>
+    <div class="row">
+        <div class="col-6">
+            <h4>About</h4>
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
         </div>
-    </footer>
+        <div class="col-3">
+            <h4>Games</h4>
+            <ul>
+                <li>PS1</li>
+                <li>PS2</li>
+                <li>PS3</li>
+                <li>PS4</li>
+            </ul>
+        </div>
+        <div class="col-3">
+            <h4>Quick Links</h4>
+            <ul>
+                <li>Home</li>
+                <li>Games</li>
+                <li>Wish list</li>
+                <li>Deals</li>
+            </ul>
+        </div>
+    </div>
+</footer>
     </body>
 </html>
