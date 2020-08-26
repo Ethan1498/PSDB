024972f48b76a075333e19df88cacddf97aa8600
Chris <chrhac13@gmail.com>
2020-08-26 11:53:31 +0100

    refactored api

--- scripts/api.php
+++ scripts/api.php
@@ -1,35 +1,52 @@
 <?php         
 header("Content-Type:application.json");
-if (isset($_GET["id"]) && !empty($_GET["id"])) { 
+if (isset($_GET["l"]) && !empty($_GET["l"])) { 
     include "connect.php";
-    $id = $_GET["id"];
-    $tab = $_GET["tab"];
+    $limit = $_GET["l"];
+    $_limit = mysqli_real_escape_string($conn, $limit); 
+    $page = $_GET["p"];
+    $_page = mysqli_real_escape_string($conn, $page);
+    $console = $_GET["c"];
+    $_console = mysqli_real_escape_string($conn, $console);
+
+    $offset = $_limit * ($_page-1);
+    
+
     $tables = array(games, ps1games, ps2games, ps3games, ps4games);
-    $table = $tables[$tab];             
-    $_id = mysqli_real_escape_string($conn, $id);            
-    $sql = "SELECT * FROM $table WHERE id=$_id";
+    $table = $tables[$_console];             
+               
+    $sql = "SELECT * FROM $table LIMIT $offset, $_limit";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result)>0){
-        $row = mysqli_fetch_array($result);
-        $id = $row["id"];
-        $image = $row["image"];
-        $title = $row["title"];
-        $price = $row["price"];
-        $oldPrice = $row["oldPrice"];
-        $console = $row["console"];
-        response($id,$image,$title,$price,$oldPrice,$console);
+        $res_arr=array();
+        $res_arr["Results"]=array();
         
+        while($row = mysqli_fetch_array($result)){
+            $id = $row["id"];
+            $image = $row["image"];
+            $title = $row["title"];
+            $price = $row["price"];
+            $oldPrice = $row["oldPrice"];
+            $console = $row["console"];
+
+            $res_item = array(
+                "id"=>$id, 
+                "image"=>$image, 
+                "title"=>$title, 
+                "price"=>$price, 
+                "oldPrice"=>$oldPrice, 
+                "console"=>$console
+            );
+            array_push($res_arr["Results"], $res_item); 
+        }
+        echo json_encode($res_arr);
                         
     } else {
-        response("No Record Found", NULL, NULL, NULL, NULL);            
+        response("No Record Found", NULL, NULL, NULL, NULL, NULL);            
     }
 } else {
-    response("Invalid Request", NULL, NULL, NULL, NULL);
+    response("Invalid Request", NULL, NULL, NULL, NULL, NULL);
 }
 
-function response($id,$image,$title,$price,$oldPrice,$console){
-    
-    $arr = array("id"=>$id, "image"=>$image, "title"=>$title, "price"=>$price, "oldPrice"=>$oldPrice, "console"=>$console);
-    echo json_encode($arr);
-}     
+     
 ?>
